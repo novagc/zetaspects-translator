63a69610d30cce7901292e8a897b91563f083dba
novagc <novagc@yandex.ru>
2025-01-03 08:16:06 +0700

    fix: change log service behavior when not inited

--- ZetAspectsTranslator/Services/Log.cs
+++ ZetAspectsTranslator/Services/Log.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using BepInEx.Logging;
 
 namespace ZetAspectsTranslator.Services;
@@ -6,23 +7,26 @@ namespace ZetAspectsTranslator.Services;
 internal static class Log
 {
     private static ManualLogSource _logSource;
-    private static bool _inited;
-
+    private static Action<object>[] _logMethods = Enumerable.Repeat((object _) => { }, 6).ToArray();
+    
     internal static void Init(ManualLogSource logSource)
     {
         _logSource = logSource;
-        _inited = true;
+        _logMethods =
+        [
+            _logSource.LogDebug,
+            _logSource.LogError,
+            _logSource.LogFatal,
+            _logSource.LogInfo,
+            _logSource.LogMessage,
+            _logSource.LogWarning,
+        ];
     }
 
-    internal static void Debug(object data) => TryExecute(_logSource.LogDebug, data);
-    internal static void Error(object data) => TryExecute(_logSource.LogError, data);
-    internal static void Fatal(object data) => TryExecute(_logSource.LogFatal, data);
-    internal static void Info(object data) => TryExecute(_logSource.LogInfo, data);
-    internal static void Message(object data) => TryExecute(_logSource.LogMessage, data);
-    internal static void Warning(object data) => TryExecute(_logSource.LogWarning, data);
-
-    private static void TryExecute(Action<object> logMethod, object data)
-    {
-        if (_inited && _logSource != null) logMethod(data);
-    }
+    internal static void Debug(object data) => _logMethods[0](data);
+    internal static void Error(object data) => _logMethods[1](data);
+    internal static void Fatal(object data) => _logMethods[2](data);
+    internal static void Info(object data) => _logMethods[3](data);
+    internal static void Message(object data) => _logMethods[4](data);
+    internal static void Warning(object data) => _logMethods[5](data);
 }
